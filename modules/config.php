1acec0b7d068911556a340b190dd4a1dc728b51b
glucy <69760367+glucyzz@users.noreply.github.com>
2023-12-02 14:46:28 +0800

    Update config.php

--- modules/config.php
+++ modules/config.php
@@ -8,6 +8,9 @@ $Types = [
     "heinuhome" => ["vmess", "trojan", "ss","vless"],
     "aries_init" => ["vmess", "trojan", "ss","vless"],
     "ednovasfree" => ["vmess", "trojan", "ss","vless"],
+    "v2ray_free_conf" => ["vmess", "trojan", "ss","vless"],
+    "vpnfail_v2ray" => ["vmess", "trojan", "ss","vless"],
+    "vpnfail_vless" => ["vmess", "trojan", "ss","vless"],
      
 ];
 ?>
